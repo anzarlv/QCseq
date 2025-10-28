3e7f3255abe57035c57febc8b7e9cf740d74b81f
anzaralvi <anzar.alvi@mail.utoronto.ca>
2025-10-28 19:58:54 -0400

    Describing the sample data provided in the package, sample_scRNAseq

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Single-cell RNA sequencing (scRNAseq) gene counts
+#'
+#' A dataset of Peripheral Blood Mononuclear Cells (PBMC) freely available from
+#' 10X Genomics. There are 2,700 single cells that were sequenced on the
+#' Illumina NextSeq 500
+#'
+#' @source 10x Genomics
+#'
+#' @format A Seurat object with 13,714 features (genes) across 2,700 samples
+#' (cells) within 1 assay
+#'
+#' @examples
+#' \dontrun{
+#' sample_scRNAseq
+#' }
+"sample_scRNAseq"
